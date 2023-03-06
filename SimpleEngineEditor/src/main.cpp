5bd495a95b0ec22ed37136cb157b8d74284c5f42
Vlad <vladbaltgl@gmail.com>
2023-03-07 01:19:48 +0300

    Add App

--- SimpleEngineEditor/src/main.cpp
+++ SimpleEngineEditor/src/main.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
 
-#include<SimpleEngineCore/Utils/test.hpp>
+#include<memory>
 
+#include<SimpleEngineCore/Application.hpp>
+
+class MyApp : public SimpleEngine::Application
+{
+	virtual void on_update() override
+	{
+		std::cout << "Update frame: " << frame++ << std::endl;
+	}
+
+	int frame = 0;
+
+};
 int main() {
-	std::cout << "Hello from Simple Engine Editor\n";
-	
-	SimpleEngine::checkGLFW();
+	auto myApp = std::make_unique<MyApp>();
+
+	int returnCode = myApp->start(1024,768,"My first App");
+
 
 	std::cin.get();
-	return 0;
+	return returnCode;
 }
