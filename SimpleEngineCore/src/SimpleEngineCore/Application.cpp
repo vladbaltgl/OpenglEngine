33d33382647f58333b08d758aef269eae4b3cb94
Vlad <vladbaltgl@gmail.com>
2023-03-07 03:40:16 +0300

    Add spdlog

--- SimpleEngineCore/src/SimpleEngineCore/Application.cpp
+++ SimpleEngineCore/src/SimpleEngineCore/Application.cpp
@@ -1,13 +1,27 @@
 
-
 #include "SimpleEngineCore/Application.hpp"
-
+#include "SimpleEngineCore/Log.hpp"
 #include <GLFW/glfw3.h>
 
 #include<iostream>
 namespace SimpleEngine {
 	Application::Application() {
+        LOG_INFO("Welcome to spdlog!");
+        LOG_ERROR("Some error message with arg: {}", 1);
+        LOG_WARN("Easy padding in numbers like {:08d}", 12);
+        LOG_CRITICAL("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+        LOG_INFO("Support for floats {:03.2f}", 1.23456);
+        LOG_INFO("Positional args are {1} {0}..", "too", "supported");
+        LOG_INFO("{:<30}", "left aligned");
 
+     
+        // change log pattern
+        spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+
+        // Compile time log levels
+        // define SPDLOG_ACTIVE_LEVEL to desired level
+        SPDLOG_TRACE("Some trace message with param {}", 42);
+        SPDLOG_DEBUG("Some debug message");
 	}
 	Application::~Application() {
 
